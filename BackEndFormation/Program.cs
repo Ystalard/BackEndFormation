8fb1e5747036c7ebb549706b20de9b265de2c02f
Ystalard <nicolas.parisse.93@gmail.com>
2024-01-12 18:34:22 +0100

    Connected the context to the database

--- BackEndFormation/Program.cs
+++ BackEndFormation/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<SelfiesContext>(options =>
 {
-    options.UseSqlServer();
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SelfiesDatabase"), sqlOptions => {});
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
