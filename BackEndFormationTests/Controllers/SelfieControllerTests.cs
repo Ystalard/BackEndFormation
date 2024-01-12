e3c5e6bfe12840a296dcb7635a8c7b1eb01b676d
Ystalard <nicolas.parisse.93@gmail.com>
2024-01-12 12:54:00 +0100

    updated test to test what we expect the controller to do, hence to return a list of selfies

---
+++ BackEndFormationTests/Controllers/SelfieControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEndFormation.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndFormation.Controllers.Tests
+{
+    [TestClass()]
+    public class SelfieControllerTests
+    {
+        #region public methods
+        [TestMethod()]
+        public void ShouldReturnLisOfSelfies()
+        {
+            //arrange
+            var controller = new SelfieController();
+
+            //act
+            var result = controller.Get();
+
+            // assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.GetEnumerator().MoveNext());
+        }
+        #endregion
+    }
+}
