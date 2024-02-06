b336c983992667e513adcd4466aaf2c302d2975f
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-06 14:31:16 +0100

    Implemented the SelfieContextfactory class.
    This class defines how to build the context from the type of DB the application should use.
    We could have different DB. Here we use sqlServer (do not forget to import the sql server package for that).

---
+++ BanckEndFormation.Core.Selfies.Infrastructures/Data/SelfiesContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using BanckEndFormation.Core.Selfies.Infrastructures.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndFormation.Core.Selfies.Infrastructures.Data
+{
+    public class SelfiesContextFactory : IDesignTimeDbContextFactory<SelfiesContext>
+    {
+        public SelfiesContext CreateDbContext(string[] args)
+        {
+            ConfigurationBuilder configurationBuilder = new();
+            configurationBuilder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "Data", "settings", "appsettings.json"));
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+            DbContextOptionsBuilder dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(configurationRoot.GetConnectionString("SelfiesDatabase"));
+
+            SelfiesContext context = new(dbContextOptionsBuilder.Options);
+
+            return context;
+        }
+    }
+}
