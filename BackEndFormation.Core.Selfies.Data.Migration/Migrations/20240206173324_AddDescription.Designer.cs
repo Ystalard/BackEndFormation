74870ddca633269ba2535d54943391506423628d
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-06 18:42:43 +0100

    Add the description property to the migration by running: dotnet ef migrations add AddDescription --startup-project . --project=..\BackEndFormation.Core.Selfies.Data.Migration

--- BackEndFormation.Core.Selfies.Data.Migration/Migrations/20240206173324_AddDescription.Designer.cs
+++ BackEndFormation.Core.Selfies.Data.Migration/Migrations/20240206173324_AddDescription.Designer.cs
@@ -4,6 +4,7 @@ using BackEndFormation.Core.Selfies.Infrastructures.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackEndFormation.Core.Selfies.Data.Migration.Migrations
 {
     [DbContext(typeof(SelfiesContext))]
-    partial class SelfiesContextModelSnapshot : ModelSnapshot
+    [Migration("20240206173324_AddDescription")]
+    partial class AddDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackEndFormation.Core.Selfies.Data.Migration/Migrations/20240206173324_AddDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndFormation.Core.Selfies.Data.Migration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescription : Microsoft.EntityFrameworkCore.Migrations.Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Selfie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Selfie");
+        }
+    }
+}
