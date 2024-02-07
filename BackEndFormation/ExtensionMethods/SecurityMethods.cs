c5ff2629c422e9289ebe91e896a315ae763ca903
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-07 10:21:02 +0100

    Added CORS policy for the origin to allow only the domain of our front website

--- BackEndFormation/ExtensionMethods/SecurityMethods.cs
+++ BackEndFormation/ExtensionMethods/SecurityMethods.cs
@@ -19,7 +19,7 @@
             {
                 options.AddPolicy(DEFAULT_POLICY, builder =>
                 {
-                    builder.AllowAnyOrigin()
+                    builder.WithOrigins("http://127.0.0.1:5500")
                            .AllowAnyHeader()
                            .AllowAnyMethod();
                 });
