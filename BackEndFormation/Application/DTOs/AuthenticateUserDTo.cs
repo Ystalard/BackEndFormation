f455c88e23494f86025a2116d47e56b35dcea36c
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-07 14:44:32 +0100

    Implemented Authenticate controller with its related DTo

---
+++ BackEndFormation/Application/DTOs/AuthenticateUserDTo.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEndFormation.Application.DTOs
+{
+    public class AuthenticateUserDTo
+    {
+        #region properties
+        public required string Login { get; set; }
+        public string? Password { get; set; }
+
+        public string? Name { get; set; }
+        public string? Token { get; set; }
+        #endregion
+    }
+}
