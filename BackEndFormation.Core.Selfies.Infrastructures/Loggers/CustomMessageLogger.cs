b03ac2b4f75648a677869a7636d47077043953c8
Ystalard <nicolas.parisse.93@gmail.com>
2024-02-08 18:00:04 +0100

    Add our custom message logger

---
+++ BackEndFormation.Core.Selfies.Infrastructures/Loggers/CustomMessageLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndFormation.Core.Selfies.Infrastructures.Loggers
+{
+    public class CustomMessageLogger : ILogger
+    {
+        #region public methods
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel != LogLevel.Trace;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: [{logLevel}] {eventId.Id} - {formatter(state, exception)}");
+        }
+        #endregion
+    }
+}
